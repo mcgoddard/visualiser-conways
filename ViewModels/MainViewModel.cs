c39a83ec34ba7d7cb39e88c426611cd3c287d6cc
Michael Goddard <mikeygiom@gmail.com>
2016-09-22 00:00:42 +0100

    Almost got visualisation working, databinding isn't quite there

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows.Input;
 using visualiser_conways.Helpers;
+using Timer = System.Timers.Timer;
 
 namespace visualiser_conways.ViewModels
 {
@@ -61,12 +64,37 @@ namespace visualiser_conways.ViewModels
             get;
             private set;
         }
+
+        public bool CanChangeDelay
+        {
+            get
+            {
+                return !playing;
+            }
+        }
+
+        public List<List<string>> States
+        {
+            get
+            {
+                if (states == null || states.Any(s => s == null))
+                {
+                    return null;
+                }
+                var st = states.Select(r => r.Select(c => c.ToString()).ToList()).ToList();
+                return st;
+            }
+        }
         #endregion
 
         #region Private members
         private string outputDir;
-        private uint delayTime;
+        private uint delayTime = 500;
         private bool playing = false;
+        private Timer playTimer;
+        private int iterNum = 0;
+        private bool[][] states;
+        private bool rendering = false;
         #endregion
 
         #region Constructors
@@ -81,32 +109,79 @@ namespace visualiser_conways.ViewModels
         #region Private methods
         private bool CanPlay (object parameter)
         {
-            return false;
+            return !playing && !String.IsNullOrEmpty(outputDir) && delayTime > 0;
         }
 
         private void Play(object parameter)
         {
-            throw new NotImplementedException();
+            iterNum = 0;
+            playing = true;
+            playTimer = new Timer(delayTime);
+            playTimer.Elapsed += PlayTimer_Elapsed;
+            playTimer.AutoReset = true;
+            playTimer.Start();
+        }
+
+        private void PlayTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (playing)
+            {
+                if (!rendering)
+                {
+                    rendering = true;
+                    // Load a frame and display
+                    var filePath = Path.Combine(outputDir, String.Format("{0}.csv", iterNum));
+                    if (File.Exists(filePath))
+                    {
+                        var file = File.ReadLines(filePath);
+                        var states = new List<bool[]>();
+                        foreach (string line in file)
+                        {
+                            var characters = line.Split(',');
+                            states.Add(characters.Select(c => Convert.ToBoolean(int.Parse(c))).ToArray());
+                        }
+                        this.states = states.ToArray();
+                        OnPropertyChanged("States");
+                        // Increment the iteration number so we load a new frame next time
+                        iterNum++;
+                    }
+                    else
+                    {
+                        playing = false;
+                        MessageBox.Show("End of simulation", String.Format("The simulation ends at interation {0}", iterNum), MessageBoxButtons.OK);
+                    }
+                    rendering = false;
+                }
+                else
+                {
+                    Console.WriteLine("Rendering time is currently longer than delay");
+                }
+            }
         }
 
         private bool CanStop(object parameter)
         {
-            return false;
+            return playing;
         }
 
         private void Stop(object parameter)
         {
-            throw new NotImplementedException();
+            playing = false;
         }
 
         private bool CanSelectFolder(object parameter)
         {
-            return true;
+            return !playing;
         }
 
         private void SelectFolder(object parameter)
         {
-            throw new NotImplementedException();
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                OutputDir = dialog.SelectedPath;
+            }
         }
         #endregion
     }
